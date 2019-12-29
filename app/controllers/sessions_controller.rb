86c443df695d6da36f353a23641738455af40029
chaitanya40 <chaitanya@ekincare.com>
2019-12-29 21:48:21 +0530

    fix class names

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
  skip_before_action :authenticate_request
 
- def authenticate
+ def create
    command = AuthenticateUser.call(params[:email], params[:password])
 
    if command.success?
