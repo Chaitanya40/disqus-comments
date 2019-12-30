6decc126775aab93a3d3de931b46c619cce0b17b
chaitanya40 <chaitanya@ekincare.com>
2019-12-30 06:37:51 +0530

    add all required controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,49 @@
 class CommentsController < ApplicationController
+	before_action :set_question
+
 	def index
+		render json: @question.comments.page(params[:page]).per(params[:per_page])
 	end
 	
 	def update
+		comment = @question.comments.find(params[:id])
+		if comment.update_attributes(comment_params)
+			render json: comment, status: :ok
+		else 
+			render json: {error: comment.errors.full_messages.to_sentence }, status: :bad_request
+		end		
 	end 
 
 	def desroy
+		comment = @question.comments.find(params[:id])
+		if comment.destroy
+			render json: comment, status: :ok
+		else 
+			render json: {error: comment.errors.full_messages.to_sentence }, status: :bad_request
+		end
 	end 
 
 	def create
+		comment = @question.comments.create(comment_params.merge(user_id: current_user.id))
+		if comment.persisted?
+			render json: comment, status: :created
+		else 
+			render json: { error: comment.errors.full_messages.to_sentence }, status: :bad_request
+		end	
 	end
 
 	def show
-	end		
+		render json: @question.comments.find(params[:id]), status: :ok
+	end	
+
+
+	private
+
+	def	set_question
+		@question = Question.find(params[:question_id])
+	end
+
+	def comment_params
+		params.require(:comment).permit(:id, :comment_text)
+	end	
 end
--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,16 +1,44 @@
 class RepliesController < ApplicationController
+	before_action :set_comment
+
 	def index
+		render json: @comment.replies, status: :ok
 	end
 	
 	def update
+		reply = @comment.replies.find(params[:id])
+		if reply.update_attributes(reply_params)
+			render json: reply, status: :ok
+		else 
+			render json: { reply.errors.full_messages.to_sentence }, status: :bad_request
+		end		
 	end 
 
 	def desroy
+		reply = @comment.replies.find(params[:id])
+		if reply.destroy
+			render json: reply, status: :ok
+		else 
+			render json: { reply.errors.full_messages.to_sentence }, status: :bad_request
+		end		
 	end 
 
 	def create
+		reply = @comment.replies.create(user_id: current_user.id)
+		if reply.persisted?
+			render json: reply, status: :ok
+		else 
+			render json: { reply.errors.full_messages.to_sentence }, status: :created
+		end		
 	end
 
 	def show
-	end		
+		render json: @comment.replies.find(params[:id]), status: :ok
+	end	
+
+	private
+
+	def set_comment
+		@comment = Comment.find(params[:id])
+	end	
 end
