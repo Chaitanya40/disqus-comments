89a80410f86076864e85d1ed5c043b689c028366
chaitanya40 <chaitanya@ekincare.com>
2019-12-30 10:06:54 +0530

    fix sepc

--- spec/commands/authorize_api_request_spec.rb
+++ spec/commands/authorize_api_request_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 RSpec.describe AuthorizeApiRequest do
 	before(:all) do
 		@user = User.create(email: 'abc@example.com', password: '12345678')
-    @auth_token = AuthenticateUser.call(@user.email, '12345678')
+    @auth_token = AuthenticateUser.call(@user.email, '12345678').result
 	end
 
   describe "authorize call" do
     it "fetches a user" do
-      expect(AuthorizeApiRequest.call).to_not be_nil
+      resp = AuthorizeApiRequest.call(ActiveSupport::HashWithIndifferentAccess.new('Authorization': @auth_token)).result
+      expect(resp).to_not be_nil
     end
   end
 end
